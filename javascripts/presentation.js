30586768df82d81b6a429583eaf9d8c6f25b157c
Thomas ZILLIOX <thomas@zilliox.me>
2015-06-25 17:44:53 +0200

    Update & restyle the presentation

--- javascripts/presentation.js
+++ javascripts/presentation.js
@@ -1,1 +1,16 @@
-bespoke.horizontal.from('article');
+require(
+  ['bespoke', 'bespoke-classes', 'bespoke-keys', 'bespoke-touch', 'bespoke-hash', 'unindentor'],
+  function(bespoke, classes, keys, touch, hash, unindentor) {
+    
+    // Bespoke
+    var deck = bespoke.from('article', [
+      classes(),
+      keys('horizontal'),
+      touch(),
+      hash()
+    ]);
+    
+    // Unindentor
+    unindentor.unindentElementList('code[class*="language-"]');
+  }
+);
